75838c26fc74f6e5cb957b35ee2de71df98e3d6f
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-07-21 22:51:28 +0200

    add header in library view

--- src/renderer/views/Library.vue
+++ src/renderer/views/Library.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="flex flex-col h-screen bg-[#FFF3E3]">
+    <div class="w-full h-[80px] flex items-center justify-between p-6 text-2xl">
+        <div>
+            <span class="font-bold mr-4">Dušan Jurkovič</span>
+            <span>Práce lidu nášho (1900—1910)</span>
+        </div>
+        <div>
+            ENG <span class="font-bold">SK</span>
+        </div>
+        
+
+    </div>
     <div class="grow">
         <!-- publikacie -->
     </div>
-    <div class="w-full h-[72px] bg-[#FCF1DF] flex items-center justify-center">
+    <div class="w-full h-[72px] bg-[#FCF1DF] flex items-center justify-center p-6">
       <svg class="inline-block h-6 w-6 fill-none" viewBox="0 0 24 24">
         <path
           d="M12.8266 12.8266H22.3649V16.7691H14.4618V18.4042H22.3649V22.3467H12.8266V12.8266ZM12.8266 2.79788L21.2021 11.1734H12.8266V2.79788ZM1.63512 1.63512H11.1734V11.1734H1.63512V1.63512ZM13.9894 1.63512H22.3649V10.0106L13.9894 1.63512ZM0 0V12.8266H11.1734V22.3649H1.63512V14.4618H0V24H24V0H0Z"
