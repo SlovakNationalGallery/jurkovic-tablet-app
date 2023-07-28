28ef655092dde248d45c53edec321907065779a8
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-07-28 12:50:03 +0200

    update header + snap side empty elements sizes

--- src/renderer/views/Library.vue
+++ src/renderer/views/Library.vue
@@ -3,13 +3,14 @@
     <div class="w-full h-[80px] flex items-center justify-between p-6 text-2xl">
       <div>
         <span class="font-bold mr-4">Dušan Jurkovič</span>
-        <span>Práce lidu nášho (1900—1910)</span>
+        <span class="mr-4">Práce lidu našeho</span>
+        <span>1905</span>
       </div>
       <div>ENG <span class="font-bold">SK</span></div>
     </div>
     <div class="grow flex items-center">
       <div class="w-full flex gap-x-11 snap-x snap-mandatory overflow-x-auto no-scrollbar pt-10">
-        <div class="snap-center shrink-0"><div class="shrink-0 w-[104px]"></div></div>
+        <div class="snap-center shrink-0"><div class="shrink-0 w-[90px]"></div></div>
         <div class="snap-center shrink-0" v-for="publication in LIBRARY">
           <div class="relative">
             <div class="absolute w-full h-full bg-black z-0 rounded-xl"></div>
@@ -17,10 +18,10 @@
           </div>
           <h2 class="text-4xl font-bold text-neutral-900 py-6">{{ publication.title }}</h2>
         </div>
-        <div class="snap-center shrink-0"><div class="shrink-0 w-[104px]"></div></div>
+        <div class="snap-center shrink-0"><div class="shrink-0 w-[90px]"></div></div>
       </div>
     </div>
-    <div class="w-full h-[72px] bg-[#FCF1DF] flex items-center justify-center p-6 text-neutral-900">
+    <div class="w-full h-[72px] flex items-center justify-center p-6 text-neutral-900">
       <Logo class="inline-block h-6 w-6" />
       <span class="ml-4 text-xl">Slovenská národná galéria</span>
     </div>
