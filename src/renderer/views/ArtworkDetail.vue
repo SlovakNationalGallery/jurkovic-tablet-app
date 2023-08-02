c851173909de4e1e44535c009f99df93273aa599
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-08-02 11:15:27 +0200

    feat: add dialog + import data

--- src/renderer/views/ArtworkDetail.vue
+++ src/renderer/views/ArtworkDetail.vue
@@ -14,20 +14,45 @@
       <div>ENG <span class="font-bold">SK</span></div>
     </div>
     <div id="container" class="w-screen h-full"></div>
+    <button
+      class="absolute bottom-8 left-8 z-[1001]"
+      @click="isPopoverOpen = !isPopoverOpen"
+    >
+      <Close class="w-12 h-12" v-if="isPopoverOpen" />
+      <Info class="w-12 h-12" v-else />
+    </button>
+    <dialog
+      :open="isPopoverOpen"
+      class="absolute rounded-xl bottom-32 left-8 border-2 border-black mx-0 p-6 w-80"
+    >
+      <article class="flex flex-col">
+        <h2 class="text-xl font-bold">{{ artwork.titleSK }}</h2>
+        <span class="text-base"
+          >({{ artwork.yearStart }}
+          {{ artwork.yearEnd ? `- ${artwork.yearEnd}` : "" }})</span
+        >
+        <p class="pt-4 text-lg leading-6">
+          {{ artwork.descriptionSK }}
+        </p>
+      </article>
+    </dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, onBeforeUnmount } from "vue";
+import { onMounted, onBeforeUnmount, ref } from "vue";
 import { loadScript, unloadScript } from "vue-plugin-load-script";
 import { useRoute, useRouter } from "vue-router";
 import ArrowUp from "../icons/ArrowUp.vue";
+import Info from "../icons/Info.vue";
+import Close from "../icons/Close.vue";
 import { GALLERY } from "../consts";
 
 const route = useRoute();
 const router = useRouter();
 const index = route.params.index;
 const artwork = GALLERY[Number(index)];
+const isPopoverOpen = ref<boolean>(false);
 
 onMounted(() => {
   loadScript("/catalogue/object2vr_player.js")
