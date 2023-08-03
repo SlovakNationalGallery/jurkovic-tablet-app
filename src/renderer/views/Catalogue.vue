2b9b6519fe1d4eb94e950db647be3b4e6d8b81ad
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-08-03 14:11:53 +0200

    feat: full translation

--- src/renderer/views/Catalogue.vue
+++ src/renderer/views/Catalogue.vue
@@ -2,8 +2,9 @@
   <div class="flex flex-col h-screen">
     <div class="w-full h-[80px] flex items-center justify-between p-6 text-2xl">
       <div>
-        <span class="font-bold mr-4">Diela Ferdiša Kostku zo SNG</span>
+        <span class="font-bold mr-4">{{ trans('worksByFerdišKostka') }}</span>
       </div>
+      <LanguageSwitcher />
     </div>
     <div class="grow columns-3 px-6">
       <div class="break-inside-avoid" v-for="(artwork, index) in GALLERY">
@@ -20,11 +21,15 @@
       class="sticky bottom-0 inset-x-0 h-[72px] flex items-center justify-center p-6"
     >
       <Logo class="inline-block h-6 w-6" />
-      <span class="ml-4 text-xl">Slovenská národná galéria</span>
+      <span class="ml-4 text-xl">{{ trans('sng') }}</span>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import LanguageSwitcher from "../components/LanguageSwitcher.vue";
 import { GALLERY } from "../consts";
 import Logo from "../icons/Logo.vue";
+import { useLang } from "../composables/lang"
+
+const { trans } = useLang()
 </script>
